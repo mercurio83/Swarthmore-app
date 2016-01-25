6391784e8aa748a594368b77b329d34aba320120
Lee Tarlin <mercurio83@gmail.com>
2016-01-24 22:52:31 -0500

    added tab and navigation screens

---
+++ Swarthmore app/NavigationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NavigationViewController.swift
+//  Swarthmore app
+//
+//  Created by Lee Tarlin on 1/24/16.
+//  Copyright © 2016 Lee Tarlin. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
