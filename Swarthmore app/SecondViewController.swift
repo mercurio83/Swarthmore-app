bcb5150370ac017af7784fdd72d452e8ffc28b87
Lee Tarlin <mercurio83@gmail.com>
2016-01-24 19:37:07 -0500

    Initial Commit

---
+++ Swarthmore app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Swarthmore app
+//
+//  Created by Lee Tarlin on 1/24/16.
+//  Copyright © 2016 Lee Tarlin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
